305bd143daf891b23eb860b33bf48e410dbc437c
ROHAN-88 <therohanshrestha6655@gmail.com>
2025-12-24 16:02:10 +0000

    Intial Commit

---
+++ main/lab_one.py
@@ -0,0 +1,31 @@
+import numpy as np 
+from cd import create_data
+# X =[ [1,2,3,2.5],
+#         [2.0,5.0,-1.0,2.0],
+#         [-1.5,2.7,3.3,-0.8]]
+
+X , y = create_data(100,3)
+
+class Layer__Dense:
+    def __init__(self,n_inputs,n_neurons):
+        self.weights = 0.10*np.random.randn(n_inputs , n_neurons)
+        self.biases = np.zeros((1,n_neurons))
+    def forward(self,inputs):
+        self.output = np.dot(inputs,self.weights) + self.biases
+        
+class Activation_ReLu:
+    def forward(self,inputs):
+        self.output = np.maximum(0,inputs)
+
+        
+
+
+
+layer1 =  Layer__Dense(2,5)
+activation1 = Activation_ReLu()
+
+layer1.forward(X)
+
+activation1.forward(layer1.output)
+
+print(activation1.output)
