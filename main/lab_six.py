37735f558e11fb2fd265d432b264d976d6d21691
ROHAN-88 <therohanshrestha6655@gmail.com>
2025-12-26 17:03:51 +0000

    Learn on normalization and exponential Funtions

--- main/lab_six.py
+++ main/lab_six.py
@@ -1,13 +1,33 @@
 import math
+import numpy as np
 
 layer_outputs = [4.8,1.21,2.385]
 
+#Exponential funtions helps so  that output is not negative without losing the meaning of negative
 #E= 2.71828182846
-E = math.e
+# E = math.e
 
-exp_value = []
+# exp_value = []
 
-for output in layer_outputs:
-    exp_value.append(E**output)
+# for output in layer_outputs:
+#     exp_value.append(E**output)
 
-print(exp_value)
+
+#----------- the above exp value done easer using numpy
+exp_value = np.exp(layer_outputs)
+
+
+# Now the normalization is done .It is done after exponentioal funtions
+
+# norm_base = sum(exp_value)
+# norm_value = []
+
+# for values in exp_value:
+#     norm_value.append(values / norm_base)
+
+#----------- the above normalization value done easer using numpy
+norm_values = exp_value / np.sum(exp_value)
+
+print("this is norm value" , norm_values)
+
+
